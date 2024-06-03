f0d1644dd467a5dceb58837fd6943f27c6c0bc56
W Cardoso <cardosu7@gmail.com>
2024-06-03 17:14:30 -0300

    slides

---
+++ aula01/aula-01-js/slide.js
@@ -0,0 +1,18 @@
+
+
+var intervalo = 5000;
+
+function slide1(){
+    document.getElementById("banner").src = "./assets/banner1.jpg"
+    setTimeout("slide2()", intervalo);
+}
+
+function slide2(){
+    document.getElementById("banner").src = "./assets/banner2.jpg"
+    setTimeout("slide3()", intervalo);
+}
+
+function slide3(){
+    document.getElementById("banner").src = "./assets/banner3.jpg"
+    setTimeout("slide1()", intervalo);
+}
